439997bc65156392f3c2be225a0f0cf8d95f4a4a
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-29 00:10:42 -0500

    numero alto

---
+++ numero_mayor.py
@@ -0,0 +1,8 @@
+n1 = 0
+n2 = 0
+n1 =int(input("ingrese primer numero: "))
+n2 =int(input("ingrese segundo numero: "))
+if n1 > n2:
+    print ("el numero mayor es: ",n1)
+else :
+    print (" el numero mayor es: ",n2)
