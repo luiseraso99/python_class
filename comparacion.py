ddb8e76545681e51e82a63a74bc2360be9dbb2b4
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-29 00:06:47 -0500

    comparacion

---
+++ comparacion.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+n1 = 0
+n2 = 0
+opc = 0
+n1 =int(input("ingrese primer numero: "))
+n2 =int(input("ingrese segundo numero: "))
+if n1 > n2:
+    print ("el numero mayor es: ",n1)
+elif n2 > n1:
+    print ("el numero mayor es: ",n2)
+elif "":
+    print ("los numeros ingresados son iguales")
+    
+
+    
