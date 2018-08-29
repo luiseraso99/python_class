8920b0bdbf65c7b27d47d1eda34d8b3c78e7864e
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-28 23:59:44 -0500

    suma de edades

---
+++ academico.py.py
@@ -0,0 +1,18 @@
+sumaedades = 0
+promedio = 0
+edad1 = 15
+persona1 = ("juan carlos arteaga")
+edad2 = 18
+persona2 =("angela maria lopez")
+edad3 = 22
+persona3 = ("roberto andres jurado")
+edad4 = 19
+persona4 = ("monica maribel hurtado")
+print ("sistema de informacion academico - promedio de edades")
+sumaedades = edad1+edad2+edad3+edad4
+print ("el estudiante",persona1," tiene ",edad1," años")
+print ("el estudiante",persona2," tiene ",edad2," años")
+print ("el estudiante",persona3," tiene ",edad3," años")
+print ("el estudiante",persona4," tiene ",edad4," años")
+promedio = sumaedades/4
+print ("el promedio de edades es: ",promedio)
