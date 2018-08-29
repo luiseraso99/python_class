0c25c1601b29d34696b016f9dd3bf49f25b6cfa7
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-29 14:05:58 -0500

    tablas de multiplicar

---
+++ tabla1.py
@@ -0,0 +1,8 @@
+#tabla de multiplicar
+#tabla = 5
+tabla = int(input("digite la tabla a multiplicar: "))
+num = 1
+while num <= 10 :
+    print (tabla, "*", num, "=", tabla * num)
+    num = num + 1
+
