4af3d4e5dc0f2bc9f4588e0b0de8c3169628fdc5
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-29 14:23:00 -0500

    numero y rango a multiplicar

---
+++ ciclo3.py
@@ -0,0 +1,9 @@
+#ciclo 3 tabla multiplicar y el rango a multiplicar
+tabla = int(input("digite la tabla a multiplicar: "))
+rango = int(input("digite el rango a multiplicar: "))
+num = 1
+while num <= 10 :
+    print (tabla, "*", num, "=", tabla * num)
+    num = num + 1
+                  
+
