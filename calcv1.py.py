c36cd249179942ae2b1ea8630966edb0bb42ac62
luiseraso99 <luisjoe9901@hotmail.com>
2018-08-29 00:03:25 -0500

    calculadora

---
+++ calcv1.py.py
@@ -0,0 +1,18 @@
+n1 = 10
+n2 = 5
+print ("n1 = 10")
+print ("n2 = 5")
+print ("calculadora bsica v1.0")
+primer_numero  = n1
+segundo_numero = n2
+suma = n1+n2
+resta = n1-n2
+multiplicacion = n1*n2
+divicion = n1/n2
+print ("**********************")
+print ("resultados")
+print ("**********************")
+print ("la suma es: ",suma)
+print ("la resta es: ",resta)
+print ("la multiplicacion es: ",multiplicacion)
+print ("la divicon es: ",divicion)
